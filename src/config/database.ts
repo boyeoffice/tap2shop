f3aeb99528e51e7f28e8c4c47fa6daa2d17f6e86
Olakunle Boye <kunlexzy@gmail.com>
2025-10-14 12:53:14 +0100

    linked env

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import path from 'node:path';
 import {
   DB_HOST,
-  // DB_NAME,
+  DB_NAME,
   DB_PASSWORD,
   DB_PORT,
   DB_USER,
@@ -10,13 +10,12 @@ import {
 
 export const DatabaseConfig: DataSourceOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
-  port: 4406,
-  username: 'tap',
-  password: 'password',
-  // database: DB_NAME,
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   synchronize: false,
-  database: 'tap2shop',
   entities: [path.join(__dirname, '..', 'app', '**', '*.entity{.ts,.js}')],
   migrations: [
     path.join(__dirname, '..', 'database', 'migrations', '*{.ts,.js}'),
